20ee3581b63c0554b55bfdd2a5c190e6763135a7
Sijin Pavithran <sijinpavithran168@gmail.com>
2020-04-02 01:37:20 +0100

    Higher Order Functions and Arrays AND Async calls

---
+++ async_JS/callbacks.js
@@ -0,0 +1,33 @@
+const posts = [
+    { title: 'Post One', body: 'This is post one'},
+    { title: 'Post Two', body: 'This is post two'}
+];
+
+function getPosts() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000); // One second delay
+}
+
+// function createPost(post) {
+//     setTimeout(() => {
+//         posts.push(post);
+//     }, 2000); // Two seconds delay
+// }
+
+
+// Callback
+function createPost(post, callback) {
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    }, 2000); // Two seconds delay
+}
+
+
+
+createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
